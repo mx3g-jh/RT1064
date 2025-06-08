e9f554d1ebe84c70937107efb09b9fb339d48ace
mx3g-jh <mx3g2728692769@gmail.com>
2025-06-09 01:19:57 +0800

    add double flash

--- src/modules/DebugPrint/PrintTask.hpp
+++ src/modules/DebugPrint/PrintTask.hpp
@@ -7,25 +7,22 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-class PrintTaskt : public Tasks
-{
+class PrintTaskt : public Tasks {
 public:
-	PrintTaskt() = default;
-	~PrintTaskt() override = default;
+    PrintTaskt() = default;
+    ~PrintTaskt() override = default;
 
-	void Init(void *handle) override
-	{
-		// 可选初始化逻辑
-	}
+    void Init(void *handle) override {
+        // 可选初始化逻辑
+    }
 
-	void CallBack(void *param) override
-	{
-		while (1) {
-			const char *name = pcTaskGetName(NULL);
-			usb_echo("Task [%s] running, param: %p\r\n", name, param);
-			vTaskDelay(1000);  // 更标准写法
-		}
-	}
+    void CallBack(void *param) override {
+        while (1) {
+            const char *name = pcTaskGetName(NULL);
+            usb_echo("Task [%s] running, param: %p\r\n", name, param);
+            vTaskDelay(pdMS_TO_TICKS(1000));  // 更标准写法
+        }
+    }
 };
 
 
